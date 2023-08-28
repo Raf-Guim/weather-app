76206da716d5736fd8bf6e16087ed416860ca7a9
Rafael Guimaraes <raf.plguimaraes@gmail.com>
2023-08-28 11:44:39 -0300

    Added function to add city to local storage

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -11,7 +11,8 @@
       <div class="flex gap-3 flex-1 justify-end">
         <i @click="toggleModal"
           class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        <i @click="addCity" v-if="route.query.preview"
+          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
       </div>
 
       <BaseModal :modalActive="modalActive" @closeModal="toggleModal">
@@ -52,12 +53,39 @@
 
 <script setup>
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { uid } from 'uid';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
 import BaseModal from './BaseModal.vue';
 
 const modalActive = ref(null);
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
 
 const toggleModal = () => {
   modalActive.value = !modalActive.value;
+};
+
+const addCity = () => {
+  if (localStorage.getItem('savedCities')) {
+    savedCities.value = JSON.parse(localStorage.getItem('savedCities'));
+  }
+
+  const locationObj = {
+    id: uid(),
+    state: route.params.state,
+    city: route.params.city,
+    coord: {
+      lat: route.params.lat,
+      lng: route.params.lng,
+    },
+  };
+
+  savedCities.value.push(locationObj);
+  localStorage.setItem('savedCities', JSON.stringify(savedCities.value));
+
+  let query = Object.assign({}, route.query);
+  delete query.preview;
+  router.replace({ query });
 };
 </script>
