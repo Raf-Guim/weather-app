d5c0cfafb90a3427d2caa175b7275c80a20607ad
Rafael Guimaraes <raf.plguimaraes@gmail.com>
2023-08-29 15:15:03 -0300

    Changing require(dotenv) from index to main.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CityView from '../views/CityView.vue'
 
-require('dotenv').config();
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
